2d017f88594d2b52a66a3893f4d080734f14c442
ianklebold <ianstgo@gmail.com>
2024-12-08 03:20:40 -0300

    Configurando rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,35 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { HomePage } from './home/home.page';
+import { TabsComponent } from './components/tabs/tabs.component';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
   },
   {
-    path: 'calendar',
-    loadChildren: () => import('./calendar/calendar.module').then( m => m.CalendarPageModule)
-  },
-  {
-    path: 'teams',
-    loadChildren: () => import('./teams/teams.module').then( m => m.TeamsPageModule)
-  },
-  {
-    path: 'dashboard',
-    loadChildren: () => import('./dashboard/dashboard.module').then( m => m.DashboardPageModule)
+    path: '',
+    component: TabsComponent,
+    children:[
+      {
+        path: 'home',
+        loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+      },
+      {
+        path: 'calendar',
+        loadChildren: () => import('./calendar/calendar.module').then( m => m.CalendarPageModule)
+      },
+      {
+        path: 'teams',
+        loadChildren: () => import('./teams/teams.module').then( m => m.TeamsPageModule)
+      },
+      {
+        path: 'dashboard',
+        loadChildren: () => import('./dashboard/dashboard.module').then( m => m.DashboardPageModule)
+      },
+    ]
   },
 ];
 
