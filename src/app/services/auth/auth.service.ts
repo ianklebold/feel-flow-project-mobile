728b7d7b91a6c293bf299d4fa3420ac9cc4a16ba
ianklebold <ianstgo@gmail.com>
2024-12-31 17:51:56 -0300

    Se cierra sesion luego de que se vence el tiempo de vida del token

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 import { Storage } from '@ionic/storage-angular';
+import { jwtDecode } from 'jwt-decode';
 import { TokenModel } from 'src/app/models/auth/token-model';
 
 @Injectable({
@@ -8,13 +10,15 @@ import { TokenModel } from 'src/app/models/auth/token-model';
 export class AuthService{
 
   private JWT_DATA_KEY = 'auth-data';
+  private tokenExpirationTimer: any;
 
-  constructor(private storage: Storage) {
+  constructor(private storage: Storage, private navController: NavController) {
     this.init();
   }
 
   async init() {
     await this.storage.create();
+    this.startTokenExpirationCheck();
   }
 
   async getJwtData(): Promise<TokenModel | null> {
@@ -31,6 +35,30 @@ export class AuthService{
 
   async logout(): Promise<void> {
     await this.clearJwtData();
+  }
+
+  async logoutAndRedirect(): Promise<void> {
+    await this.logout();
+    this.navController.navigateRoot('login');
+  }
+
+  private startTokenExpirationCheck() {
+    this.tokenExpirationTimer = setInterval(async () => {
+      const jwtData: TokenModel = await this.storage.get(this.JWT_DATA_KEY);
+      if (jwtData) {
+        const expirationDate = this.getTokenExpirationDate(jwtData.token);
+        const currentTime = Math.floor(Date.now() / 1000);
+        if (currentTime >= expirationDate) {
+          await this.logoutAndRedirect();
+        }
+        console.log(expirationDate);
+      }
+    }, 60000);
+  }
+
+  private getTokenExpirationDate(token: string): number {
+    const decoded: any = jwtDecode(token);
+    return decoded.exp;
   }
 
 
